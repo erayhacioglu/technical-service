546c948c534293c01ae1351078ab1680b4e599ae
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-02-28 17:42:05 +0300

    test from Suat

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,12 @@
- 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Hello from Eray </h1>
 
-         <h1>Hello from Eray </h1>
-
-         
-
+        <h2>Hi from Suat</h2>
       </header>
     </div>
   );
